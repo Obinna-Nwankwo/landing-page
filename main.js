6e65b153a0abe738c7dbacaf51cfc4fb4d673630
Nwankwo-Official-1 <Official.Nwankwo.840@gmail.com>
2024-02-17 17:08:42 +0100

    simple landing page

---
+++ main.js
@@ -0,0 +1,9 @@
+var btn = document.getElementById("btn");
+var light = document.getElementById("light");
+var nom = d0cument.getElementById("name");
+
+function toggleBtn() {
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+    nom.classList.toggle("on");
+}
